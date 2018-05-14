7533573901ade8fc87839ec5456f6b465ddd36ce
TomHuang <tom_huangzhubin@163.com>
2018-05-14 10:14:06 +0800

    Modified the main funciton

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String name="Tom Huang";
+        System.out.println("Hello World!"+name);
     }
 }
